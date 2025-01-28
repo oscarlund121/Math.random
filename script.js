b45ff4e07c3f1496e9103d4c4acd484f9de5870b
Oscar <albertlund121@gmail.com>
2025-01-28 09:13:19 +0100

    gæt et tal

---
+++ script.js
@@ -0,0 +1,37 @@
+const min = 1;
+const max = 100;
+
+const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+console.log("Random Number: ", randomNumber)
+
+document.querySelector("button").addEventListener("click", evaluateTextField)
+
+function evaluateTextField() {
+  console.log("KLIK", )
+
+  const brugerInput = document.querySelector(".userNumber").value;
+  const brugerSvar = document.querySelector(".text");
+
+  const brugerGæt = Number(brugerInput);
+
+  if (brugerInput === randomNumber) {
+    brugerSvar.textContent = "Du gættede rigtigt!";
+  } else if (brugerInput > randomNumber) {
+    brugerSvar.textContent = "For højt! Prøv igen.";
+  } else {
+    brugerSvar.textContent = "For lavt! Prøv igen.";
+  }
+
+//   if (brugerInput < randomNumber) {
+//   brugerSvar.textContent = "For lavt! Prøv igen";
+// } else if (brugerInput > randomNumber) {
+//   brugerSvar.textContent = "For højt! Prøv igen.";
+// } else {
+//   brugerSvar.textContent = "Du gættede rigtigt!";
+// }
+};
+
+
+
+
+
